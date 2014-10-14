1ee05aa5078a08aa33edf1eeec7d05c79c014caf
Ryan Bridges <rybridges90@gmail.com>
2014-10-14 01:38:47 -0500

    working on sending http request

--- src/com/example/testgeocode/MainActivity.java
+++ src/com/example/testgeocode/MainActivity.java
@@ -1,12 +1,80 @@
 package com.example.testgeocode;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void pinLocation(View view) {
+		EditText text = (EditText) findViewById(R.id.main_input);
+		Toast.makeText(this, "Okay: " + text.getText(), Toast.LENGTH_LONG)
+				.show();
+		String text2 = text.getText().toString().replace(" ","&");
+		
+		String uri = "http://maps.google.com/maps/api/geocode/json?address="
+				+ text2+"&" + "&skey=AIzaSyAR49II0pkWe2QG6RO-hLwQW4bRS9-g94o";
+		HttpGet httpGet = new HttpGet(uri);
+		HttpClient client = new DefaultHttpClient();
+		HttpResponse response;
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy); 
+		try {
+			response = client.execute(httpGet);
+			HttpEntity entity = response.getEntity();
+			InputStream stream = entity.getContent();
+			int b;
+			while ((b = stream.read()) != -1) {
+				stringBuilder.append((char) b);
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject = new JSONObject(stringBuilder.toString());
+
+			double lng = ((JSONArray) jsonObject.get("results"))
+					.getJSONObject(0).getJSONObject("geometry")
+					.getJSONObject("location").getDouble("lng");
+
+			double lat = ((JSONArray) jsonObject.get("results"))
+					.getJSONObject(0).getJSONObject("geometry")
+					.getJSONObject("location").getDouble("lat");
+
+//			Log.d("latitude", "" + lat);
+//			Log.d("longitude", "" + lng);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
